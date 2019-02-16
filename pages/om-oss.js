c154df290ac5dd81f8b77cd0f5f8f3d097d07434
Axel Pettersson <axel@pettersson.cc>
2019-02-16 21:25:36 +0100

    Content update

--- pages/om-oss.js
+++ pages/om-oss.js
@@ -1,6 +1,26 @@
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
+import InfoIcon from "@material-ui/icons/Info";
 
 export default () => (
-  <div>
-  </div>
-)
+  <Container>
+    <div className="text-center font-weight-light">
+      <InfoIcon style={{ fontSize: "2.5rem" }} />
+      <h1>Om oss</h1>
+    </div>
+    <p>
+      Partiguiden.nu skapades med syftet av att kunna erbjuda en plattform där
+      man lätt kan kolla upp partiernas ståndpunkter i olika ämnen och
+      sakfrågor. Under utvecklingen av hemsidan togs det hänsyn till
+      begriplighet och användarvänlighet för att kunna erbjuda den bästa möjliga
+      användarupplevensen.
+    </p>
+    <p>
+      Utvecklingen av sidan påbörjades som ett fritidsprojekt år 2017 för att
+      utveckla kunskaper inom webbutveckling.
+    </p>
+    <p>
+      Informationen som presenteras på sidan är utvald information från
+      partiernas egna hemsidor.
+    </p>
+  </Container>
+);

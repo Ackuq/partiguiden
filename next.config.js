2d5d4dc54b974266d37a59f443df706e822d62a0
Axel Pettersson <axel@pettersson.cc>
2019-02-26 00:43:43 +0100

    Google Cloud integration

--- next.config.js
+++ next.config.js
@@ -1,22 +1,17 @@
 const withCSS = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
 const webpack = require("webpack");
-const getRoutes = require("./lib/routes");
-const pathmap = require("./lib/pathmap");
 
 require("dotenv").config();
 
 module.exports = withCSS(
   withSass({
-    exportPathMap: pathmap.pathmap,
     webpack(config) {
       const env = Object.keys(process.env).reduce((acc, curr) => {
         acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
         return acc;
       }, {});
-
       config.plugins.push(new webpack.DefinePlugin(env));
-
       return config;
     }
   })
--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const routes = require("./lib/routes");
 const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 
+const port = process.env.PORT || 8080;
 // Use express
 const express = require("express");
 app.prepare().then(() => {
   express()
     .use(handler)
-    .listen(3000);
+    .listen(port);
 });

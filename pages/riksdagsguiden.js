7689bb7a481ad8ba452c9e6b52e018d984de668e
Axel Pettersson <axel@pettersson.cc>
2019-03-06 14:27:10 +0100

    Start of riksdagsguiden!

---
+++ pages/riksdagsguiden.js
@@ -0,0 +1,14 @@
+/* Next.js Head component */
+import Head from "next/head";
+
+export default () => (
+  <div className="page-content">
+    <Head>
+      <title>Riksdagsguiden | Partiguiden.nu</title>
+    </Head>
+    <div className="list-title text-center">
+      <h1>Riksdagsguiden</h1>
+    </div>
+    <div className="container" />
+  </div>
+);

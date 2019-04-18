51f422934feb4ec76d4920d368b762f8d756b277
Axel Pettersson <axel@pettersson.cc>
2019-04-18 02:26:13 +0200

    Added google analytics and  recharts

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,8 @@ import getPageContext from "../lib/getPageContext";
 /* Header and footer components*/
 import Header from "../components/LayoutComponents/Header";
 import Footer from "../components/LayoutComponents/Footer";
+/* Google analytics */
+import ReactGA from "react-ga";
 
 /* Scroll up button */
 import ToTopButton from "../components/LayoutComponents/ToTopButton";
@@ -26,7 +28,13 @@ export default class MyApp extends App {
 
   pageContext = null;
 
+  initReactGA() {
+    ReactGA.initialize("UA-111642551-2");
+    ReactGA.pageview(window.location.pathname);
+  }
+
   componentDidMount() {
+    this.initReactGA();
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentNode) {

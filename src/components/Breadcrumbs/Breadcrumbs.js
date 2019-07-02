385ccaffb53f4d7fee2ca7f4e521be9551770a64
Axel <axel@pettersson.cc>
2019-07-02 21:52:16 +0200

    Fixed style bug

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { emphasize, withStyles, makeStyles } from '@material-ui/core/styles';
+import { emphasize, withStyles, styled } from '@material-ui/core/styles';
 import { Breadcrumbs, Chip, Avatar } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import { array } from 'prop-types';
@@ -21,38 +21,36 @@ const StyledBreadcrumb = withStyles(theme => ({
   }
 }))(Chip);
 
-const useStyles = makeStyles(theme => ({
-  avatar: {
-    background: 'none',
-    marginRight: -theme.spacing(1.5)
-  }
+const CustomAvatar = styled(Avatar)(({ theme }) => ({
+  background: 'none',
+  color: '#616161',
+  width: '32px',
+  height: '32px',
+  fontSize: '1rem',
+  marginRight: -theme.spacing(1.5)
 }));
 
-const CustomBreadcrumbs = ({ links }) => {
-  const classes = useStyles();
-
-  return (
-    <Breadcrumbs aria-label="Breadcrumb">
-      <Link href="/">
-        <StyledBreadcrumb
-          component="a"
-          href="/"
-          label="Hem"
-          avatar={
-            <Avatar className={classes.avatar}>
-              <HomeIcon />
-            </Avatar>
-          }
-        />
+const CustomBreadcrumbs = ({ links }) => (
+  <Breadcrumbs aria-label="Breadcrumb">
+    <Link href="/">
+      <StyledBreadcrumb
+        component="a"
+        href="/"
+        label="Hem"
+        avatar={
+          <CustomAvatar>
+            <HomeIcon />
+          </CustomAvatar>
+        }
+      />
+    </Link>
+    {links.map(link => (
+      <Link href={link.href} key={link.label}>
+        <StyledBreadcrumb component="a" href={link.href} label={link.label} />
       </Link>
-      {links.map(link => (
-        <Link href={link.href} key={link.label}>
-          <StyledBreadcrumb component="a" href={link.href} label={link.label} />
-        </Link>
-      ))}
-    </Breadcrumbs>
-  );
-};
+    ))}
+  </Breadcrumbs>
+);
 
 CustomBreadcrumbs.propTypes = {
   links: array.isRequired

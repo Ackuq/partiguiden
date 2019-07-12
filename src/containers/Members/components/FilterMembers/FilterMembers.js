9e9fa882cd39b3cd5630cc0d0fcd95b5d046a718
Axel <axel@pettersson.cc>
2019-07-12 06:34:44 +0200

    Added divider in member filtering component

--- src/containers/Members/components/FilterMembers/FilterMembers.js
+++ src/containers/Members/components/FilterMembers/FilterMembers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Divider } from '@material-ui/Core';
 
 import FilterContainer, {
   useFilter,
@@ -27,6 +28,7 @@ const FilterMembers = () => {
   return (
     <FilterContainer>
       <FilterSearch updateSearch={updateSearch} search={search} />
+      <Divider />
       <FilterList
         list={list}
         isChecked={isChecked}

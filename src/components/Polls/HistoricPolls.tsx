b89c2016702b235088c95f0ebef7af6c09c625e7
Ackuq@ <axel@pettersson.cc>
2021-03-16 12:35:05 +0100

    Improve historic polls

--- src/components/Polls/HistoricPolls.tsx
+++ src/components/Polls/HistoricPolls.tsx
@@ -1,9 +1,23 @@
 import { Paper, styled, Typography } from '@material-ui/core';
-import React from 'react';
-import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import React, { useState } from 'react';
+import {
+  Brush,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import { MonthlyAverage } from '../../lib/polls';
-import { partyAbbreviations } from '../../types/party.d';
+import { partyAbbrev, partyAbbreviations } from '../../types/party.d';
 import { partiesMap } from '../../utils/getParties';
+
+const ChartContainer = styled(ResponsiveContainer)({
+  marginTop: '1rem',
+  marginLeft: '-20px',
+});
 
 const PollCard = styled(Paper)({
   padding: '1rem 0.5rem',
@@ -15,22 +29,48 @@ interface Props {
 }
 
 const HistoricPolls: React.FC<Props> = ({ historicPolls }) => {
+  const [hide, setHide] = useState<Array<partyAbbrev>>([]);
+
+  const hideParty = (party: partyAbbrev) => {
+    setHide((prev) => [...prev, party]);
+  };
+
+  const showParty = (party: partyAbbrev) => {
+    setHide((prev) => prev.filter((p) => p !== party));
+  };
+
+  const onClick = (event: React.MouseEvent<Element, MouseEvent>) => {
+    const party = ((event as unknown) as { value: partyAbbrev }).value;
+    if (hide.includes(party)) {
+      showParty(party);
+    } else {
+      hideParty(party);
+    }
+  };
+
   return (
     <PollCard>
       <Typography variant="h5" align="center">
         Historisk genomsnitt (senaste 4 åren)
       </Typography>
-      <ResponsiveContainer height={500}>
+      <ChartContainer height={500}>
         <LineChart data={historicPolls}>
           <XAxis type="category" dataKey="date" />
           <YAxis type="number" />
           <Tooltip />
-          <Legend />
+          <Legend wrapperStyle={{ marginLeft: 30, bottom: -5 }} onClick={onClick} />
           {partyAbbreviations.map((party) => (
-            <Line key={party} dataKey={party} stroke={partiesMap[party].color} dot={false} />
+            <Line
+              key={party}
+              dataKey={party}
+              hide={hide.includes(party)}
+              stroke={partiesMap[party].color}
+              dot={false}
+            />
           ))}
+          <Brush dataKey="date" style={{ marginBottom: 20 }} />
         </LineChart>
-      </ResponsiveContainer>
+      </ChartContainer>
     </PollCard>
   );
 };

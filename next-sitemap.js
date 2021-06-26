4ac99af9a09825025a1129d6778687a52a4ee272
Ackuq@ <axel@pettersson.cc>
2021-06-26 16:55:45 +0200

    Disallow indexing of everything non-prod

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,12 @@
+const robotsTxtPolicy =
+  process.env.VERCEL_ENV === 'production'
+    ? [{ userAgent: '*', allow: '/' }]
+    : [{ userAgent: '*', disallow: '/' }];
+
 module.exports = {
   siteUrl: process.env.BASE_PATH || 'https://partiguiden.se',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: robotsTxtPolicy,
+  },
 };

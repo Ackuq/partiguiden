458a2327e7e254af522ea16b0103b4193da656eb
Ackuq@ <axel@pettersson.cc>
2021-06-28 21:37:40 +0200

    Disable logging for PWA

---
+++ worker/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+'use strict';
+
+// To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
+// https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
+self.__WB_DISABLE_DEV_LOGS = true;

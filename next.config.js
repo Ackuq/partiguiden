e397f826470094009a9bb2d34c123c91f6554c87
Ackuq@ <axel@pettersson.cc>
2021-07-19 00:16:47 +0200

    Re-enable PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withSourceMaps = require('@zeit/next-source-maps');
 const { withSentryConfig } = require('@sentry/nextjs');
-/* const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache'); */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
@@ -29,30 +29,36 @@ const {
   RESPONSIVE_AD_SLOT,
 } = process.env;
 
-const moduleExports = withBundleAnalyzer(
-  withSourceMaps({
-    env: {
-      NEXT_PUBLIC_COMMIT_SHA: VERCEL_GITHUB_COMMIT_SHA,
-      API_URL,
-      BASE_PATH,
-      PROXY_URL,
-      VERCEL_ENV,
+const moduleExports = withPWA(
+  withBundleAnalyzer(
+    withSourceMaps({
+      pwa: {
+        dest: 'public',
+        runtimeCaching,
+      },
+      env: {
+        NEXT_PUBLIC_COMMIT_SHA: VERCEL_GITHUB_COMMIT_SHA,
+        API_URL,
+        BASE_PATH,
+        PROXY_URL,
+        VERCEL_ENV,
 
-      // Sentry
-      SENTRY_DSN,
-      SENTRY_URL: 'https://sentry.io/',
-      SENTRY_ORG,
-      SENTRY_PROJECT,
-      SENTRY_AUTH_TOKEN,
+        // Sentry
+        SENTRY_DSN,
+        SENTRY_URL: 'https://sentry.io/',
+        SENTRY_ORG,
+        SENTRY_PROJECT,
+        SENTRY_AUTH_TOKEN,
 
-      // Google AdSense
-      AD_CLIENT_ID,
-      FLOW_AD_SLOT,
-      RESPONSIVE_AD_SLOT,
-    },
+        // Google AdSense
+        AD_CLIENT_ID,
+        FLOW_AD_SLOT,
+        RESPONSIVE_AD_SLOT,
+      },
 
-    basePath: '',
-  })
+      basePath: '',
+    })
+  )
 );
 
 const SentryWebpackPluginOptions = {

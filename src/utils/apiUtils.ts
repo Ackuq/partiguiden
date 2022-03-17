2d76ec4ac820db043fe623ffcd95bb2ea4efe292
Axel Pettersson <axel@pettersson.cc>
2022-03-17 22:05:40 +0100

    Fix caching

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,5 +1,5 @@
 import { NextApiResponse } from 'next';
 
 export const setCache = (time: number, res: NextApiResponse) => {
-  res.setHeader('Cache-Control', `max-age=0, s-maxage=${time}`);
+  res.setHeader('Cache-Control', `max-age=${time}, public`);
 };

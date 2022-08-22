3237347c597130518e358ca6018f08208b5af337
Axel Pettersson <axel@pettersson.cc>
2022-08-22 19:38:36 +0200

    Only enable tracking in production

--- src/lib/fbPixel.ts
+++ src/lib/fbPixel.ts
@@ -1,10 +1,14 @@
 export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
 
 export const pageview = () => {
-  window.fbq('track', 'PageView');
+  if (process.env.NODE_ENV === 'production') {
+    window.fbq('track', 'PageView');
+  }
 };
 
 // https://developers.facebook.com/docs/facebook-pixel/advanced/
 export const event = (eventName: string, options: Record<string, unknown> = {}) => {
-  window.fbq('track', eventName, options);
+  if (process.env.NODE_ENV === 'production') {
+    window.fbq('track', eventName, options);
+  }
 };

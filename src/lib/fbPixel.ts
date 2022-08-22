17b8b61d332fc10d37f06d9e3bb6b5b79501ddd4
Axel Pettersson <axel@pettersson.cc>
2022-08-22 23:21:25 +0200

    Ignore errors thrown by trackers

--- src/lib/fbPixel.ts
+++ src/lib/fbPixel.ts
@@ -2,13 +2,21 @@ export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
 
 export const pageview = () => {
   if (process.env.NODE_ENV === 'production') {
-    window.fbq('track', 'PageView');
+    try {
+      window.fbq('track', 'PageView');
+    } catch (error) {
+      // Ignore error
+    }
   }
 };
 
 // https://developers.facebook.com/docs/facebook-pixel/advanced/
 export const event = (eventName: string, options: Record<string, unknown> = {}) => {
   if (process.env.NODE_ENV === 'production') {
-    window.fbq('track', eventName, options);
+    try {
+      window.fbq('track', eventName, options);
+    } catch (error) {
+      // Ignore error
+    }
   }
 };
--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -3,9 +3,13 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || '';
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
   if (process.env.NODE_ENV === 'production') {
-    window.gtag('config', GA_TRACKING_ID, {
-      page_path: url,
-    });
+    try {
+      window.gtag('config', GA_TRACKING_ID, {
+        page_path: url,
+      });
+    } catch (error) {
+      // Ignore error
+    }
   }
 };
 
@@ -19,10 +23,14 @@ interface GTagEvent {
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent): void => {
   if (process.env.NODE_ENV === 'production') {
-    window.gtag('event', action, {
-      event_category: category,
-      event_label: label,
-      value,
-    });
+    try {
+      window.gtag('event', action, {
+        event_category: category,
+        event_label: label,
+        value,
+      });
+    } catch (error) {
+      // Ignore error
+    }
   }
 };

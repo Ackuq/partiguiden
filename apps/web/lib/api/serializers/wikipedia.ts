24e24aaf14cf9fb93fff95758f1ac8f321134e79
Axel Pettersson <axel@pettersson.cc>
2023-09-24 19:54:07 +0200

    Test js-dom removal

--- apps/web/lib/api/serializers/wikipedia.ts
+++ apps/web/lib/api/serializers/wikipedia.ts
@@ -1,4 +1,3 @@
-import { JSDOM } from "jsdom";
 import type { Leader } from "@lib/api/types/member";
 import type { WikipediaInfoBox } from "@lib/api/types/wikipedia";
 import { getMemberQuery } from "../controllers/members";
@@ -26,6 +25,7 @@ export const getAbstract = (data: WikipediaAbstractResponse): string => {
   return abstract;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getLeader = (
   firstName: string,
   lastName: string,
@@ -58,72 +58,10 @@ interface WikipediaInfoBoxResponse {
 }
 
 export const getInfoBoxAttr = async (
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   data: WikipediaInfoBoxResponse,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   party: Party,
 ): Promise<WikipediaInfoBox> => {
-  const dom = new JSDOM(data.parse.text["*"]);
-
-  const rowHeaders = dom.window.document.body.getElementsByTagName("th");
-  let website = "";
-  const leaders: Leader[] = [];
-  const ideology: string[] = [];
-
-  // eslint-disable-next-line no-restricted-syntax
-  for (const header of rowHeaders) {
-    /* Remove spaces since HTML spaces are treated differently */
-    const title = header.textContent?.replace(/[[\s]/g, "");
-    const item = header.nextSibling;
-
-    switch (title) {
-      case "Politiskideologi":
-        // eslint-disable-next-line no-restricted-syntax
-        if (!item) {
-          break;
-        }
-        for (const ideologyNode of item.childNodes) {
-          if (
-            ideologyNode.nodeName !== "A" ||
-            ideologyNode.textContent === null
-          ) {
-            continue;
-          }
-          ideology.push(ideologyNode.textContent);
-        }
-        break;
-      case "Partiledare":
-      case "Partiordförande":
-      case "Partisekreterare":
-      case "Gruppledare":
-      case "Språkrör":
-        if (!item) {
-          break;
-        }
-        for (const leaderNode of item.childNodes) {
-          const name = leaderNode.textContent;
-          if (leaderNode.nodeName !== "A" || !name) {
-            continue;
-          }
-          const [firstName, ...lastName] = name
-            .replace(/\[\d+\]/g, "")
-            .split(" ");
-          leaders.push(
-            await getLeader(firstName, lastName.join(" "), title, party),
-          );
-        }
-        break;
-      case "Webbplats":
-        // Website is defined on the next row
-        const siteElement = header.parentElement?.nextElementSibling;
-        if (!siteElement) {
-          continue;
-        }
-        website = (
-          siteElement.querySelector("a")?.getAttribute("href") ??
-          siteElement.textContent ??
-          ""
-        ).replace("http://", "https://");
-    }
-  }
-
-  return { ideology, website, leaders };
+  return { ideology: [], website: "", leaders: [] };
 };

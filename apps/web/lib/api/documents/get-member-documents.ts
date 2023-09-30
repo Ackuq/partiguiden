9de50bc2063c05c14f4c4fccb6171d51fb39fd95
Axel Pettersson <axel@pettersson.cc>
2023-09-30 14:26:25 +0200

    Refactor member document query

--- apps/web/lib/api/documents/get-member-documents.ts
+++ apps/web/lib/api/documents/get-member-documents.ts
@@ -7,8 +7,17 @@ interface Query {
 }
 
 export default async function getMemberDocuments({ id, page }: Query) {
+  const query = new URLSearchParams({
+    iid: id,
+    p: page.toString(),
+    avd: "dokument",
+    sort: "datum",
+    sortorder: "datum",
+    utformat: "json",
+  });
+
   const response = await fetch(
-    `${PARLIAMENT_BASE_URL}/dokumentlista/?avd=dokument&sort=datum&sortorder=datum&utformat=json&iid=${id}&p=${page}`,
+    `${PARLIAMENT_BASE_URL}/dokumentlista/?${query}`,
     { next: { revalidate: 60 * 60 * 24 } },
   );
   const data = await response.json();

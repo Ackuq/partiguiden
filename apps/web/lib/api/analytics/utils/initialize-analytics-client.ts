96e7fbe1b922235f18fe6fd62444dcbe67856916
Axel Pettersson <axel@pettersson.cc>
2023-10-08 19:23:46 +0200

    Use base64

--- apps/web/lib/api/analytics/utils/initialize-analytics-client.ts
+++ apps/web/lib/api/analytics/utils/initialize-analytics-client.ts
@@ -1,18 +1,17 @@
 import { BetaAnalyticsDataClient } from "@google-analytics/data";
 
-const SERVICE_ACCOUNT_PRIVATE_KEY = process.env.GA4_SERVICE_ACCOUNT_PRIVATE_KEY;
-const SERVICE_ACCOUNT_CLIENT_EMAIL =
-  process.env.GA4_SERVICE_ACCOUNT_CLIENT_EMAIL;
+const SERVICE_ACCOUNT = process.env.GA4_SERVICE_ACCOUNT;
 
 export default function initializeAnalyticsClient() {
-  if (!SERVICE_ACCOUNT_PRIVATE_KEY || !SERVICE_ACCOUNT_CLIENT_EMAIL) {
+  if (!SERVICE_ACCOUNT) {
     console.warn("GA4 Service account not set");
     return;
   }
+
+  const buffer = Buffer.from(SERVICE_ACCOUNT, "base64");
+  const credentials = JSON.parse(buffer.toString("utf-8"));
+
   return new BetaAnalyticsDataClient({
-    credentials: {
-      client_email: SERVICE_ACCOUNT_CLIENT_EMAIL,
-      private_key: SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    },
+    credentials,
   });
 }

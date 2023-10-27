2368dfaffd3133696cefee462b1bfb738c84e505
Axel Pettersson <axel@pettersson.cc>
2023-10-27 08:23:25 +0200

    Fix breaking changes

--- apps/web/lib/image/icon.tsx
+++ apps/web/lib/image/icon.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from "next/server";
+import { ImageResponse } from "next/og";
 import colors from "tailwindcss/colors";
 
 export default async function xPartiguidenIcon(size: number) {

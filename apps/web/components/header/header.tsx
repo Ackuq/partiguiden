190c82889a5476bb5a92e2a8be743c626c19ee28
Axel Pettersson <axel@pettersson.cc>
2024-05-21 19:33:43 +0200

    ui: Improve header responsiveness

--- apps/web/components/header/header.tsx
+++ apps/web/components/header/header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
       <div className="bg-teal-700 h-header sm:h-header-sm absolute w-full dark:bg-slate-800"></div>
       <header className="h-header sm:h-header-sm sticky top-0 z-10 flex shadow-md">
         <div className="bg-teal-700/80 flex h-full w-full flex-col text-slate-50 backdrop-blur-md dark:bg-slate-800/75">
-          <div className="container flex flex-1 items-center">
+          <div className="px-4 sm:px-20 flex flex-1 items-center">
             <MainLogo />
             <div className="ml-auto flex gap-4">
               <ThemeToggle />

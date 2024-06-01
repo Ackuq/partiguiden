3f0fe37ad075705d34a12235ea20c56fa586b1e3
Axel Pettersson <axel@pettersson.cc>
2024-06-01 22:32:02 +0200

    chore: One single eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { fixupConfigRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+const config = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    name: "Disable type checking for JS files",
+    files: ["**/*.js", "**/*.mjs"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  //...fixupConfigRules(flatCompat.extends("next/core-web-vitals")),
+  {
+    languageOptions: {
+      parserOptions: {
+        project: [
+          "./tsconfig.json",
+          "./apps/*/tsconfig.json",
+          "./packages/*/tsconfig.json",
+        ],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
+
+export default config;

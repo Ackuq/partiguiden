d0346edb794ac7b6c43f42bb3adb51f68537e1eb
Axel Pettersson <axel@pettersson.cc>
2024-06-02 23:45:15 +0200

    Add git helpers

---
+++ actions/update-standpoints/src/git-helper.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+import { getExecOutput } from "@actions/exec";
+
+/**
+ * Create a new branch
+ * @param {string} branch
+ * @param {string} startPoint
+ */
+export async function checkout(branch, startPoint) {
+  const command = ["checkout", "-b", branch, startPoint];
+  await exec(command);
+}
+
+export async function add() {
+  const command = ["add", "."];
+  await exec(command);
+}
+
+/**
+ * @param {string} message
+ */
+export async function commit(message) {
+  const command = ["commit", "-m", message];
+  await exec(command);
+}
+
+export async function push() {
+  const command = ["push", "--set-upstream", "origin", "HEAD"];
+  await exec(command);
+}
+
+export async function hasDiff() {
+  const command = ["diff", "--quiet"];
+  const output = await exec(command);
+  // Exit code 1 means there are differences
+  return output.exitCode === 1;
+}
+
+/**
+ * Run a git command
+ * @param {string[]} command
+ */
+function exec(command) {
+  return getExecOutput("git", command);
+}

5cb2d665e1ca4a4212026df94a6354853bbf502b
Axel Pettersson <axel@pettersson.cc>
2024-06-02 23:17:52 +0200

    Add boilerplate for action

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,20 +4,28 @@ import tseslint from "typescript-eslint";
 import baseConfig from "./tooling/eslint/base.mjs";
 import nextConfig from "./tooling/eslint/next.mjs";
 
-const config = tseslint.config(...baseConfig, ...nextConfig, {
-  linterOptions: {
-    reportUnusedDisableDirectives: true,
-  },
-  languageOptions: {
-    parserOptions: {
-      project: [
-        "./apps/web/tsconfig.json",
-        "./tsconfig.json",
-        "./packages/*/tsconfig.json",
-      ],
-      tsconfigRootDir: import.meta.dirname,
+const config = tseslint.config(
+  ...baseConfig,
+  ...nextConfig,
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: [
+          "./apps/web/tsconfig.json",
+          "./tsconfig.json",
+          "./packages/*/tsconfig.json",
+        ],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-});
+  {
+    name: "Ignore actions dist",
+    ignores: ["actions/*/dist"],
+  },
+);
 
 export default config;

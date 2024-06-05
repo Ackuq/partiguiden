c0b9982057944e47388314d7e569a4702251b961
Axel Pettersson <axel@pettersson.cc>
2024-06-06 01:14:03 +0200

    chore: Add env variable declarations

--- tooling/eslint/types.d.ts
+++ tooling/eslint/types.d.ts
@@ -1,3 +1,12 @@
+declare module "eslint-plugin-turbo" {
+  import type { Linter, Rule } from "eslint";
+
+  export const configs: {
+    recommended: { rules: Linter.RulesRecord };
+  };
+  export const rules: Record<string, Rule.RuleModule>;
+}
+
 declare module "eslint-plugin-import" {
   import type { Linter, Rule } from "eslint";
 

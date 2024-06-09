7b563d2965cd189642a414e418cff4395e09f5f6
Axel Pettersson <axel@pettersson.cc>
2024-06-09 18:06:57 +0200

    Do some more styling

--- apps/web/app/dokument/[id]/page.tsx
+++ apps/web/app/dokument/[id]/page.tsx
@@ -1,5 +1,3 @@
-import { twMerge } from "tailwind-merge";
-
 import Container from "@components/common/container";
 import PageTitle from "@components/common/page-title";
 import SocialMediaShare from "@components/common/social-media-share";
@@ -27,13 +25,7 @@ export default async function Document({ params: { id } }: Props) {
       </PageTitle>
       <Container>
         <SocialMediaShare title={`${data.id}: ${data.title}`} />
-        <div
-          dangerouslySetInnerHTML={{ __html: html }}
-          id="document-content"
-          className={twMerge(
-            "dark:[&_*]:!border-slate-50 dark:[&_*]:!text-slate-50",
-          )}
-        />
+        <div dangerouslySetInnerHTML={{ __html: html }} id="document-content" />
       </Container>
     </main>
   );

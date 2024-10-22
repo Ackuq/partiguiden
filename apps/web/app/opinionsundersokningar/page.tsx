f19f539962a71a69ff97d4eeeb728af77159bb88
Axel Pettersson <axel@pettersson.cc>
2024-10-22 22:13:32 +0200

    Fix compilation errors

--- apps/web/app/opinionsundersokningar/page.tsx
+++ apps/web/app/opinionsundersokningar/page.tsx
@@ -1,67 +1,10 @@
 import { ChartBarIcon } from "@heroicons/react/24/solid";
-import dynamic from "next/dynamic";
-import { twMerge } from "tailwind-merge";
 
-import { ResponsiveAd } from "@components/ads";
-import { Card } from "@components/common/card";
 import Container from "@components/common/container";
 import PageTitle from "@components/common/page-title";
 import getPolls from "@lib/api/polls/get-polls";
 
-const LOADING_BARS = [
-  "h-80",
-  "h-64",
-  "h-60",
-  "h-72",
-  "h-48",
-  "h-96",
-  "h-32",
-  "h-56",
-];
-
-const BlockStatistics = dynamic(() => import("./components/block-statistics"), {
-  ssr: false,
-  loading: () => (
-    <div
-      role="status"
-      className="h-52 w-full animate-pulse bg-slate-200 dark:bg-slate-900 sm:h-80"
-    />
-  ),
-});
-const BlockBuilder = dynamic(() => import("./components//block-builder"), {
-  ssr: false,
-  loading: () => (
-    <div
-      role="status"
-      className="h-[43rem] w-full animate-pulse bg-slate-200 dark:bg-slate-900"
-    />
-  ),
-});
-const HistoricPolls = dynamic(() => import("./components//historic-polls"), {
-  ssr: false,
-  loading: () => (
-    <div
-      role="status"
-      className="h-[30rem] w-full animate-pulse bg-slate-200 dark:bg-slate-900"
-    />
-  ),
-});
-const MonthPoll = dynamic(() => import("./components//month-poll"), {
-  loading: () => (
-    <div role="status" className="ml-[40px] flex h-96 items-end sm:h-[30rem]">
-      {LOADING_BARS.map((height) => (
-        <div
-          key={height}
-          className={twMerge(
-            "mx-2 flex-1 animate-pulse bg-slate-200 dark:bg-slate-900",
-            height,
-          )}
-        />
-      ))}
-    </div>
-  ),
-  ssr: false,
-});
+import Graphs from "./graphs";
 
 export const metadata = {
   title: "Opinionsundersökningar | Partiguiden",
@@ -80,36 +23,11 @@ export default async function PollPage() {
       <PageTitle Icon={ChartBarIcon}>Opinionsundersökningar</PageTitle>
 
       <Container className="flex flex-col gap-4">
-        <Card>
-          <h4 className="mb-3 text-center text-xl sm:text-2xl">
-            Senaste mätningar
-          </h4>
-          <MonthPoll currentMonthAverage={currentMonthAverage} />
-        </Card>
-        <ResponsiveAd />
-        <Card>
-          <h4 className="mb-3 text-center text-xl sm:text-2xl">
-            Historiskt genomsnitt (senaste 4 åren)
-          </h4>
-          <HistoricPolls historicPolls={historicPolls} />
-        </Card>
-        <Card>
-          <h4 className="mb-3 text-center text-xl sm:text-2xl">
-            Bygg din egna regering
-          </h4>
-          <BlockBuilder currentMonthAverage={currentMonthAverage} />
-        </Card>
-        <Card className="overflow-visible">
-          <h4 className="mb-3 text-center text-xl sm:text-2xl">
-            Blockskillnad (senaste mätningar)
-          </h4>
-          <BlockStatistics
-            currentMonthAverage={currentMonthAverage}
-            blockAverage={blockAverage}
-          />
-        </Card>
-
-        <ResponsiveAd />
+        <Graphs
+          currentMonthAverage={currentMonthAverage}
+          historicPolls={historicPolls}
+          blockAverage={blockAverage}
+        />
       </Container>
     </main>
   );
--- apps/web/components/ads/index.ts
+++ apps/web/components/ads/index.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 import LoadingFlowAd from "@components/loading/flow-ad";

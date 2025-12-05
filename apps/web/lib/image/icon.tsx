011ed7cda133042a68ff45c95fb51ebf3bdec817
Axel Pettersson <axel@pettersson.cc>
2025-12-05 11:52:12 +0100

    Format files

--- apps/web/lib/image/icon.tsx
+++ apps/web/lib/image/icon.tsx
@@ -8,22 +8,20 @@ export default async function PartiguidenIcon(size: number) {
   );
 
   return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: size * 0.6,
-          fontWeight: 700,
-          color: colors.teal[700],
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        PG
-      </div>
-    ),
+    <div
+      style={{
+        fontSize: size * 0.6,
+        fontWeight: 700,
+        color: colors.teal[700],
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      PG
+    </div>,
     {
       width: size,
       height: size,

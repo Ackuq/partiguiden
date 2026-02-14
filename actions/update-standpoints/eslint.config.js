43a2bff1cde7815378f1f0759d1ba9c0f8207f57
Axel Pettersson <apettersson@spotify.com>
2026-02-14 11:40:39 +0100

    Format

--- actions/update-standpoints/eslint.config.js
+++ actions/update-standpoints/eslint.config.js
@@ -2,6 +2,9 @@ import { defineConfig, globalIgnores } from "eslint/config";
 
 import baseConfig from "@partiguiden/eslint-config/base";
 
-const config = defineConfig(globalIgnores(["dist/**", ".rollup.cache/**"]), baseConfig);
+const config = defineConfig(
+  globalIgnores(["dist/**", ".rollup.cache/**"]),
+  baseConfig,
+);
 
 export default config;

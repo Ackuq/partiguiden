d1754f045efc2020b4fc1bc18fadc51bf366241c
Axel Pettersson <axel@pettersson.cc>
2026-08-01 15:30:13 +0200

    Update L scrapers

--- packages/scrapers/src/party/l-scraper.ts
+++ packages/scrapers/src/party/l-scraper.ts
@@ -4,6 +4,9 @@ export default class LScraper extends Scraper {
   baseUrl = "https://www.liberalerna.se";
   listPath = "/politik-a-o/";
   listSelector = ".politicsIdx-list-group a";
-  opinionTags = [".spolitik-content.container > .wysiwyg-content p"];
+  opinionTags = [
+    "div.ftdcontent-inner .wysiwyg-content > ul > li",
+    ".spolitik-content.container > .wysiwyg-content p",
+  ];
   absoluteUrls = true;
 }

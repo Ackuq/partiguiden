0982fca6d15d4485b56b70a7f4123ad75ec1d9ae
Axel Pettersson <axel@pettersson.cc>
2026-08-15 00:55:18 +0200

    Update SD scraper and categorize

--- packages/scrapers/src/party/sd-scraper.ts
+++ packages/scrapers/src/party/sd-scraper.ts
@@ -1,98 +1,57 @@
-import * as pdfjs from "pdfjs-dist/legacy/build/pdf.mjs";
+import * as cheerio from "cheerio";
 
 import type { PartyDataWithoutPartyName } from "@partiguiden/party-data/types";
 
 import Scraper from "../scraper.ts";
 
-type SectionDestination = [
-  { num: number; gen: number },
-  { name: string },
-  number,
-  number,
-  number,
-];
+interface SDItem {
+  id: number;
+  title: string;
+  link: string;
+  content: string;
+  excerpt: string;
+  utskott: string[];
+}
 
-const IGNORED_SECTIONS = ["Innehåll", "Inledning"];
+function cleanText(content: string): string[] {
+  return content
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => !!line);
+}
 
 export default class SDScraper extends Scraper {
-  baseUrl =
-    "https://sd.se/wp-content/uploads/2022/07/sverigedemokraternas-valplattform-2022-april.pdf";
-  listPath = "";
-  listSelector = "";
-  titleTag = "";
+  baseUrl = "https://www.sd.se";
+  listPath = "/a-o/";
+  listSelector = "#ao-matters-az-data";
 
-  async getPages(): Promise<PartyDataWithoutPartyName[]> {
-    const pdf = await pdfjs.getDocument({
-      url: this.baseUrl,
-      useSystemFonts: true,
-    }).promise;
-    const outline = await pdf.getOutline();
-    const dataWithPageIndexPromises = outline.map(async (section) => {
-      const title = section.title;
-      const destination = section.dest as SectionDestination;
-      let pageIndex = await pdf.getPageIndex(destination[0]);
-      if (title === "HBT+") {
-        // The page index is wrong for this entry
-        pageIndex++;
-      }
-      return { url: `${this.baseUrl}#page=${pageIndex + 1}`, title, pageIndex };
+  async getPages(limit?: number): Promise<PartyDataWithoutPartyName[]> {
+    const response = await this.fetchPage(this.baseUrl + this.listPath, {
+      headers: { "Content-Type": "text/plain; charset=UTF-8" },
     });
-    const dataWithPageIndex = await Promise.all(dataWithPageIndexPromises);
-    const filteredDataWithPageIndex = dataWithPageIndex.filter(
-      (data) => !IGNORED_SECTIONS.includes(data.title),
-    );
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const rawJson = $(this.listSelector).html();
 
-    filteredDataWithPageIndex.sort((a, b) => {
-      if (a.pageIndex > b.pageIndex) {
-        return 1;
-      }
-      if (a.pageIndex < b.pageIndex) {
-        return -1;
-      }
-      return 0;
-    });
-    // Transform page indexes into array of page numbers
-    const dataWithPages = filteredDataWithPageIndex.map((data, index) => {
-      const nextPageIndex =
-        index !== filteredDataWithPageIndex.length - 1
-          ? filteredDataWithPageIndex[index + 1].pageIndex
-          : pdf.numPages;
+    if (!rawJson) {
+      throw new Error("Script container #ao-matters-az-data not found.");
+    }
+
+    let items = JSON.parse(rawJson) as SDItem[];
+
+    if (limit) {
+      items = items.toSpliced(limit);
+    }
+
+    const parsedData = items.map<PartyDataWithoutPartyName>((item) => {
       return {
-        title: data.title,
-        url: data.url,
-        pageNumbers: Array.from(
-          { length: nextPageIndex - data.pageIndex },
-          (_, i) => i + data.pageIndex + 1,
-        ),
+        title: item.title,
+        url: item.link,
+        updateDate: new Date().toISOString(),
+        opinions: cleanText(item.content),
       };
     });
-    // Resolve the pages and map out the opinions
-    const dataWithStandpointsPromises = dataWithPages.map(
-      async (data): Promise<PartyDataWithoutPartyName> => {
-        const pageContentPromises = data.pageNumbers.map(async (pageNumber) => {
-          const page = await pdf.getPage(pageNumber);
-          const textContent = await page.getTextContent();
-          return textContent.items
-            .map((content) => ("str" in content ? content.str : ""))
-            .join("");
-        });
-        const pageContent = (await Promise.all(pageContentPromises)).join("");
-        const opinionsString = pageContent.split(
-          "Sverigedemokraterna vill:",
-        )[1];
-        return {
-          title: data.title,
-          url: data.url,
-          opinions: opinionsString
-            .split("•")
-            .map((opinion) => opinion.trim())
-            .filter((opinion) => opinion !== ""),
-          updateDate: new Date().toISOString(),
-          subject: undefined,
-        };
-      },
-    );
-    const dataWithStandpoints = await Promise.all(dataWithStandpointsPromises);
-    return dataWithStandpoints;
+
+    return parsedData;
   }
 }
